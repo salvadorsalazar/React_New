67e8cf512cc4fafb98afd8ccbec1bb77e51a56c0
salvadorsalazar <salvador.g.salazar79@gmail.com>
2022-11-04 23:25:55 -0500

    updates

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,19 @@ const db = mysql.createConnection({
     password: "codeup",
     database:"test"
 }) 
+
+app.get("/", (req, res) => {
+    res.json("hello this is the backend")
+})
+
+app.get("/books", (req, res) => {
+    const q = "SELECT * FROM  books"
+    db.query(q, (err, data) => {
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+
+})
 app.listen(8800, ()=> {
     console.log("Connected to backend !")
 
