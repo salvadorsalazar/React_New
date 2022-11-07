5afa9248e3578d5a500f34025ebff67b5fcc631c
salvadorsalazar <salvador.g.salazar79@gmail.com>
2022-11-06 21:31:00 -0600

    updates

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import mysql from "mysql";
+import cors from "cors";
 
-const app = express()
+const app = express();
+app.use(cors());
+app.use(express.json());
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
