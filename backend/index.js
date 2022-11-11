3761338ce5b911af014bb8bb0c181c407019c122
salvadorsalazar <salvador.g.salazar79@gmail.com>
2022-11-10 23:23:04 -0600

    fixed mysql auth problem

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
 import express from "express";
 import mysql from "mysql";
-import cors from "cors";
+// import cors from "cors";
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 const db = mysql.createConnection({
     host: "localhost",
@@ -11,6 +11,9 @@ const db = mysql.createConnection({
     password: "codeup",
     database:"test"
 }) 
+
+// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'codeup';
+// 
 
 app.get("/", (req, res) => {
     res.json("hello this is the backend")
@@ -25,49 +28,6 @@ app.get("/books", (req, res) => {
         return res.json(data);
     });
 
-});
-
-app.post("/books", (req, res) => {
-  const q = "INSERT INTO books(`title`, `desc`, `price`, `cover`) VALUES (?)";
-
-  const values = [
-    req.body.title,
-    req.body.desc,
-    req.body.price,
-    req.body.cover,
-  ];
-
-  db.query(q, [values], (err, data) => {
-    if (err) return res.send(err);
-    return res.json(data);
-  });
-});
-
-app.delete("/books/:id", (req, res) => {
-  const bookId = req.params.id;
-  const q = " DELETE FROM books WHERE id = ? ";
-
-  db.query(q, [bookId], (err, data) => {
-    if (err) return res.send(err);
-    return res.json(data);
-  });
-});
-
-app.put("/books/:id", (req, res) => {
-  const bookId = req.params.id;
-  const q = "UPDATE books SET `title`= ?, `desc`= ?, `price`= ?, `cover`= ? WHERE id = ?";
-
-  const values = [
-    req.body.title,
-    req.body.desc,
-    req.body.price,
-    req.body.cover,
-  ];
-
-  db.query(q, [...values,bookId], (err, data) => {
-    if (err) return res.send(err);
-    return res.json(data);
-  });
 });
 
 app.listen(8800, () => {
